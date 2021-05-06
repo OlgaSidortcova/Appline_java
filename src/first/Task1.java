96deda7e0a3d0e24bc860f0961cec7d983d2135e
olga <olga-24_@mail.ru>
2021-05-06 18:14:37 +0300

    Распределила задачи по классам

---
+++ src/first/Task1.java
@@ -0,0 +1,8 @@
+package first;
+
+public class Task1 {
+
+    public static void hello() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/first/Task2.java
@@ -0,0 +1,11 @@
+package first;
+
+public class Task2 {
+    public static void second_lab() {
+        double a = 3.14;
+        System.out.println(a);
+        byte b = Byte.MAX_VALUE;
+        System.out.println(b);
+
+    }
+}
