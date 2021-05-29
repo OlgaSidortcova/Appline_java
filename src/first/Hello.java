e417283b41deab6b1cba91db319f9a682db9d3bd
olga <olga-24_@mail.ru>
2021-05-29 15:45:29 +0300

    Исправленный ответ на задание 13 в Task13.java

--- src/first/Hello.java
+++ src/first/Hello.java
@@ -9,16 +9,11 @@ public class Hello {
         //Task6.task6();
         //Task5.task5();
         //Task2.second_lab();
-        //Test2.arr1();
+        Task13.task13();
         //Test2.test();
         //  Task12.task12("I like Java!!!");
-        Task13.task13();
-        /*boolean a = 2==1+1 | true;
-        System.out.println(a);
-        if (true && false){
-            System.out.println(true && false);
-        }System.out.println(true && false);
-        */
+       // Fin_1.fin1();
+
 
     }
 
--- src/first/Task13.java
+++ src/first/Task13.java
@@ -10,14 +10,15 @@ public class Task13 {
         String first = sc.nextLine();
 
         String[] result = first.split(" ");
-
+        int count = 0;
         System.out.println("Слова только на латинице:");
         for (String elem : result) {
             boolean onlyLatinAlphabet = elem.matches("^[a-zA-Z]+$");
             if (onlyLatinAlphabet) {
                 System.out.print(elem + " ");
+                count++;
             }
         }
-        System.out.println("\nВсего слов в строке  " + result.length);
+        System.out.println("\nВсего слов в строке  " + count);
     }
 }
